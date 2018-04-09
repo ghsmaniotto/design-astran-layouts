41572897d740e2aa01b79cf037f4ace6e90b8e76
Gustavo Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2018-04-09 10:00:34 -0300

    Add checks to given arguments

--- design_layouts.sh
+++ design_layouts.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
-# Create folders
+# Get parameters
+
+if [ "$1" == "--help" ]; then
+	echo "To run type:"
+	echo "./design_layouts.sh --astran <path_to_astran_project> --spices <path_to_spices>"
+	exit 1
+elif [ "$1" == "--astran" ] && [ "$3" == "--spices" ]; then
+	if [ -d $2 ] && [ -d $4 ]; then
+		ASTRAN_PATH=$2
+		SPICES_PATH=$4
+		echo "--> Correct arguments."
+	else
+		echo "--> --astran and/or --spices values are not directories."
+	fi
+else
+	echo "--> Invalid arguments."
+	exit 1
+fi
+
+
+# Get spices
+SPICES=(${SPICES_PATH}/*.sp)
+echo $SPICES
+NUM_SPICES=${#SPICES[@]}
+echo $NUM_SPICES
+
+# Check if there are spices in folder
+if [[ -f ${SPICES[0]} ]]; then
+	echo "--> $NUM_SPICES spices were founded."
+else
+	echo "--> The spices folders does not contain spice files."
+	exit 1
+fi
+
+
+# Create auxiliar folders
 mkdir -p in_execution
 mkdir -p final/spices
 mkdir -p final
@@ -8,7 +43,7 @@ mkdir -p final/layouts
 mkdir -p final/spices
 mkdir -p initial
 mkdir -p final/spices
-echo "The necessary folders were created"
+echo "--> The auxiliar folders were created."
 
 # Define OS type and get number of cores
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -18,15 +53,19 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     OS="macos"
     CORES=$( sysctl -n hw.ncpu )
 else
-	echo "The Operation System can't be defined. The script end."
+	echo "--> The Operation System can't be defined. The script end."
 	exit 1
 fi
 
-echo "The operation system is $OS. The machine has $CORES logic cores."
+echo "--> The operation system is $OS. The machine has $CORES logic cores."
 
 
+echo "Do you like to design how many layouts of each spice?"
+read LAYOUTS_PER_SPICE
 
-
+if [[ $LAYOUTS_PER_SPICE =~ ^[0-9]+$ ]]; then
+	echo $LAYOUTS_PER_SPICE	
+fi
 
 
 # FILES=/home/astran-master/Astran/build/bin/TCC_Design_Layouts/Reordering/design_spices/pclass-4-fc/original/v2/spices/*.sp #Pasta com arquivos .sp das celulas

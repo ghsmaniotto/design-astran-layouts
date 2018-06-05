45cb7d63a32f71d617e56a7bc9db3623f7558f26
ghsmaniotto <ghsmaniotto@inf.ufpel.edu.br>
2018-06-05 15:50:11 -0300

    Compact old files. And remove unnecessary comments.

--- design_layouts.sh
+++ design_layouts.sh
@@ -21,8 +21,9 @@ elif [ "$1" == "--astran" ] && [ "$3" == "--spices" ] && [ "$5" == "--rules" ] &
 		echo "-----> Tech rules: $TECH_RULES"
 		echo "-----> Gurobi license: $GUROBI_LIC"
 		echo "-----> Cores to run: ${10}"
-		echo "--> Correct arguments."
+		echo "--> The arguments are correct."
 	else
+		echo "### ERROR:"
 		echo "### --astran and/or --spices values are not directories."
 		echo "### --rules and/or --gurobi_lic values are not files."
 	fi
@@ -41,29 +42,23 @@ echo $NUM_SPICES
 if [[ -f ${SPICES[0]} ]]; then
 	echo "--> $NUM_SPICES spices were founded."
 else
+	echo "### ERROR:"
 	echo "### The spices folders does not contain spice files."
 	exit 1
 fi
 
-# Define OS type and get number of cores
-# if [[ "$OSTYPE" == "linux-gnu" ]]; then
-#     OS="linux"
-#     CORES=$( nproc )
-# elif [[ "$OSTYPE" == "darwin"* ]]; then
-#     OS="macos"
-#     CORES=$( sysctl -n hw.ncpu )
-# else
-# 	echo "### The Operation System can't be defined. The script end."
-# 	exit 1
-# fi
-
-# echo "--> The operation system is $OS. The machine has $CORES logic cores."
-
 # Get execution path
 STR=$0
 
-# Remove folder that was in execution
+# Compress the folders to able new simulation
+DATE="$(date +%m_%d_%y__%k_%M)"
+zip -r ${DATE} in_execution not_designed final initial
+
+# Remove folders
 rm -fR ${STR/\/.\/*/''}/in_execution
+rm -fR ${STR/\/.\/*/''}/not_designed
+rm -fR ${STR/\/.\/*/''}/final
+rm -fR ${STR/\/.\/*/''}/initial
 
 # Create auxiliar folders in execution folder
 mkdir -p ${STR/\/.\/*/''}/in_execution
@@ -77,6 +72,7 @@ mkdir -p ${STR/\/.\/*/''}/final/spices
 
 # Copy the spices to in_execution folder
 cp ${SPICES_PATH}/*.sp ${STR/\/.\/*/''}/initial
+
 echo "--> Copy the spice files to in_execution directory."
 echo "${SPICES_PATH}/*.sp to ${STR/\/.\/*/''}/initial"
 # Creates directories for each core
@@ -86,17 +82,7 @@ for (( CORE = 0; CORE < ${#CORES[@]}; CORE++ )); do
 	mkdir -p ${STR/\/.\/*/''}/in_execution/${CORES[$CORE]}/spices
 	mkdir -p ${STR/\/.\/*/''}/in_execution/${CORES[$CORE]}/layouts
 done
-
 echo "--> The auxiliar folders were created."
-# echo "Do you like to design how many layouts of each spice? Must be decimal."
-# read LAYOUTS_PER_SPICE
-
-# if [[ $LAYOUTS_PER_SPICE =~ ^[0-9]+$ ]]; then
-# 	echo $LAYOUTS_PER_SPICE
-# else
-# 	echo "### Invalid layouts per spice."
-# 	exit 1
-# fi
 
 # Creates docker container running run_docker_astran.sh script
 for (( CORE = 0; CORE < ${#CORES[@]}; CORE++ )); do
@@ -109,7 +95,5 @@ for (( CORE = 0; CORE < ${#CORES[@]}; CORE++ )); do
 	   -v $TECH_RULES:/home/techrule.rul \
 	   -v ${STR/\/.\/*/''}:/home/simulation \
 	   --net=host ghsmaniotto/astran:1.0.0 \
-	   sh -c "cd /home/simulation && chmod 777 run_docker_astran.sh && ./run_docker_astran.sh --astran /home/astran/Astran/build/bin --rules /home/techrule.rul --gurobi_lic /opt/gurobi.lic --core ${CORES[$CORE]}" 
-
+	   sh -c "cd /home/simulation && chmod 777 run_docker_astran.sh && ./run_docker_astran.sh --astran /home/astran/Astran/build/bin --rules /home/techrule.rul --gurobi_lic /opt/gurobi.lic --core ${CORES[$CORE]}"
 done
-
